feadea1ac9160124426e26b87211e68c9d946d19
Jayden <jrmunger199@gmail.com>
2021-05-29 21:55:36 +0200

    main

--- app/src/main/java/com/example/findmyway/Settings.java
+++ app/src/main/java/com/example/findmyway/Settings.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,12 +22,15 @@ public class Settings extends AppCompatActivity {
     Button _continue;
     Intent intent;
     Preferences preferences;
+    RadioGroup prefLandmarks, prefUnits;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        prefLandmarks = (RadioGroup) findViewById(R.id.rgPreflandmark);
+        prefUnits = (RadioGroup) findViewById(R.id.rgPrefDistance);
         historical = findViewById(R.id.rbHistorical);
         modern = findViewById(R.id.rbModern);
         popular = findViewById(R.id.rbPopular);
@@ -46,7 +50,6 @@ public class Settings extends AppCompatActivity {
 
             String selectedPrefLandmark;
             String selectedPrefDistance;
-
             if(historical.isSelected()){
                  selectedPrefLandmark = "Historical";
             }else if(modern.isSelected()){
@@ -64,6 +67,24 @@ public class Settings extends AppCompatActivity {
             preferences = new Preferences(selectedPrefLandmark, selectedPrefDistance, email);
             Firebasedb.child(id).setValue(preferences);
 
+            prefLandmarks.setOnCheckedChangeListener((group, checkedId) -> {
+                   //find which radioButton is checked by id
+                if(checkedId == R.id.rbHistorical) {
+                    prefLandmark.setText("You chose Historical");
+                } else if(checkedId == R.id.rbModern) {
+                    prefLandmark.setText("You chose Modern");
+                } else {
+                    prefLandmark.setText("You chose Popular");
+                }
+            });
+            prefUnits.setOnCheckedChangeListener((group, checkedId) -> {
+                // find which radioButton is checked by id
+                if(checkedId == R.id.rbMetric) {
+                    prefDistance.setText("You chose metric units");
+                } else {
+                    prefDistance.setText("You chose imperial units");
+                }
+            });
             startActivity(new Intent(Settings.this, Profile.class));
 
         });
