006f3584318ed44526a26eaaaff6f06d73ffe12d
Jayden <jrmunger199@gmail.com>
2021-05-30 16:20:44 +0200

    adding display for  user profile

--- app/src/main/java/com/example/findmyway/Profile.java
+++ app/src/main/java/com/example/findmyway/Profile.java
@@ -5,20 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
 
 import org.jetbrains.annotations.NotNull;
 
 public class Profile extends AppCompatActivity {
 
-    EditText address,Fname, Lname;
-    DatabaseReference reference;
+    EditText address,Fname, Lname, Flocation, landPref, unitPref;
+    DatabaseReference reference,referencePref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +30,34 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
          reference = FirebaseDatabase.getInstance().getReference("User");
-         Fname = findViewById(R.id.editTextFname);
-         Lname = findViewById(R.id.editTextLname);
-         address = findViewById(R.id.editTextAddress);
+         referencePref = FirebaseDatabase.getInstance().getReference("User_Pref");
+         Fname = findViewById(R.id.txtFirstname);
+         Lname = findViewById(R.id.txtLastname);
+         address = findViewById(R.id.txtAddress);
+         Flocation = findViewById(R.id.txtFavlocations);
+         landPref = findViewById(R.id.txtLandPref);
+         unitPref = findViewById(R.id.txtLandPref);
+         FirebaseUser user;
+         String uid;
+         user = FirebaseAuth.getInstance().getCurrentUser();
+         uid = user.getUid();
+            //ArrayList<String> arrayList = new ArrayList<>();
 
-         reference.orderByChild("f_Name").equalTo("james").addListenerForSingleValueEvent(new ValueEventListener() {
+         reference.addValueEventListener(new ValueEventListener() {
              @Override
-             public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+             public void onDataChange(DataSnapshot snapshot) {
                  for(DataSnapshot datas: snapshot.getChildren()){
-                     String Fnamedb=datas.child("f_Name").getValue().toString();
-                     String Lnamedb = datas.child("l_Name").getValue().toString();
-                     String addressdb = datas.child("address").getValue().toString();
-
-                     Fname.setText(Fnamedb);
-                     Lname.setText(Lnamedb);
-                     address.setText(addressdb);
-
-                     Fname.setEnabled(false);
-                     Lname.setEnabled(false);
-                     address.setEnabled(false);
-                 }
+                 String user_name = datas.child(uid).child("f_name").getValue(String.class);
+                 String user_surname = datas.child(uid).child("l_name").getValue(String.class);
+                 String user_address = datas.child(uid).child("address").getValue(String.class);
+                 String user_preflandmark = datas.child(uid).child("prefLandmark").getValue(String.class);
+                 String user_prefdistance = datas.child(uid).child("prefdistance").getValue(String.class);
+                 Fname.setText(user_name);
+                 Lname.setText(user_surname);
+                 //address.setText(user_address);
+                 landPref.setText(user_preflandmark);
+                 unitPref.setText(user_prefdistance);
+             }
              }
 
              @Override
