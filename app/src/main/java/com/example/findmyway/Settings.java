afe92b31e6c1deff400887ec65d757c9182d7a43
James <jamestygoddard@gmail.com>
2021-05-30 13:57:58 +0200

    wip: fixed git

--- app/src/main/java/com/example/findmyway/Settings.java
+++ app/src/main/java/com/example/findmyway/Settings.java
@@ -51,7 +51,7 @@ public class Settings extends AppCompatActivity {
             String selectedPrefLandmark;
             String selectedPrefDistance;
             if(historical.isChecked()){
-                 selectedPrefLandmark = "Historical test";
+                 selectedPrefLandmark = "Historical";
             }else if(modern.isChecked()){
                 selectedPrefLandmark = "Modern";
             }else{
