ddbebcb390310f1c44efc4a4abe1ddd932bb44a7
James <jamestygoddard@gmail.com>
2021-06-03 20:08:55 +0200

    wip: got the maps profile button working wip: got the maps add fav working need further work

--- app/src/main/java/com/example/findmyway/Maps.java
+++ app/src/main/java/com/example/findmyway/Maps.java
@@ -6,15 +6,18 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.FileUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
@@ -61,18 +64,21 @@ import android.widget.Toast;
 
 
 public class Maps extends AppCompatActivity implements OnMapReadyCallback {
+
     private GoogleMap mMap;
     private static final String TAG = "MainActivity";
     private boolean mLocationPermissionGranted = false;
     private FusedLocationProviderClient mFusedLocationProviderClient;
     private static final float DEFAULT_ZOOM = 15f;
-    public String searchString;
+    //public String searchString;
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
-    public String plce;
+    //public String plce;
     public Place places;
 
+    private ImageView profile, favLocation;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +86,12 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         getLocationPermission();
+
+        profile = findViewById(R.id.ic_profile);
+        favLocation = findViewById(R.id.ic_favlocation);
+
+        intent = getIntent();
+        String email = intent.getStringExtra("Email_Key");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
         AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
@@ -93,13 +105,11 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
             Places.initialize(getApplicationContext(), "AIzaSyD8HIfCfFq12-QWwvcX05YHsdyZ_8jushg");
         }
 
-// Create a new Places client instance.
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
                 places = place;
-            geoLocate();
-            //addMarker(places);
+                geoLocate();
             }
 
 
@@ -109,6 +119,12 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
             }
         });
 
+        //gets profile activity
+        Profile(email);
+
+        // add fav location
+        // Need to add email to add favorite list when saving the fav location
+        AddFavLocation(email,places);
     }
 
     @Override
@@ -127,11 +143,8 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
             }
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setMyLocationButtonEnabled(false);
-
-
         }
     }
-
     private void getDeviceLocation() {
         Log.d(TAG, "getDeviceLocation: getting the devices current location");
 
@@ -159,18 +172,14 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
             Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
         }
     }
-
     private void moveCamera(LatLng latLng, float zoom) {
         Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
 
     }
-
     public void init() {
         Log.d(TAG, "init: initializing");
-        //geoLocate();
     }
-    //WIP
     private void geoLocate() {
        Log.d(TAG, "geoLocate: locating");
         String searchstring = this.places.getName();
@@ -193,72 +202,68 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
             mMap.addMarker(mp);
         }
     }
-            private void getLocationPermission () {
-                Log.d(TAG, "getLocationPermission: getting location permissions");
-                String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.ACCESS_COARSE_LOCATION};
+    private void getLocationPermission () {
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
 
-                if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                        FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                            COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        mLocationPermissionGranted = true;
-                        initMap();
-                    } else {
-                        ActivityCompat.requestPermissions(this,
-                                permissions,
-                                LOCATION_PERMISSION_REQUEST_CODE);
-                    }
-                } else {
-                    ActivityCompat.requestPermissions(this,
-                            permissions,
-                            LOCATION_PERMISSION_REQUEST_CODE);
-                }
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+                initMap();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
             }
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+    private void initMap () {
+        Log.d(TAG, "initMap: initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(Maps.this);
+    }
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions,
+    @NonNull int[] grantResults){
+        Log.d(TAG, "onRequestPermissionsResult: called.");
+        mLocationPermissionGranted = false;
 
-            private void initMap () {
-                Log.d(TAG, "initMap: initializing map");
-                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                        .findFragmentById(R.id.map);
-                mapFragment.getMapAsync(Maps.this);
-            }
-            @Override
-            public void onRequestPermissionsResult ( int requestCode, @NonNull String[] permissions,
-            @NonNull int[] grantResults){
-                Log.d(TAG, "onRequestPermissionsResult: called.");
-                mLocationPermissionGranted = false;
-
-                switch (requestCode) {
-                    case LOCATION_PERMISSION_REQUEST_CODE: {
-                        if (grantResults.length > 0) {
-                            for (int i = 0; i < grantResults.length; i++) {
-                                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
-                                    mLocationPermissionGranted = false;
-                                    Log.d(TAG, "onRequestPermissionsResult: permission failed");
-                                    return;
-                                }
-                            }
-                            Log.d(TAG, "onRequestPermissionsResult: permission granted");
-                            mLocationPermissionGranted = true;
-                            //initialize our map
-                            initMap();
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                            return;
                         }
                     }
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    mLocationPermissionGranted = true;
+                    //initialize our map
+                    initMap();
                 }
             }
+        }
+    }
+    private void Profile(String email){
+        profile.setOnClickListener(v -> {
+            Intent passSetting =new Intent(Maps.this, Profile.class);
+            passSetting.putExtra("Email_Key", email);
+            startActivity(passSetting);
+        });
+    }
+    private void AddFavLocation(String email,Place places){
+        favLocation.setOnClickListener(v -> Toast.makeText(Maps.this, "UserEmail" +email + " User favorite location saved; Location:"+places, Toast.LENGTH_SHORT).show());
 
-//            public void addMarker (Place p){
-//
-//                MarkerOptions markerOptions = new MarkerOptions();
-//
-//                markerOptions.position(p.getLatLng());
-//                markerOptions.title(p.getName() + "");
-//                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
-//
-//                mMap.addMarker(markerOptions);
-//                mMap.moveCamera(CameraUpdateFactory.newLatLng(p.getLatLng()));
-//                mMap.animateCamera(CameraUpdateFactory.zoomTo(13));
-//            }
-
+    }
     }
 
